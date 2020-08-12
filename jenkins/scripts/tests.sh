f9134fd95cafbda5e675f8f3bdb18b94074479c3
Todd Nguyen <todd.nguyen47@gmail.com>
2020-08-12 09:33:51 -0700

    Move scripts into separate windows/unix folders

--- jenkins/scripts/tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-reports_folder="reports"
-
-# This file should be executed in the root folder where `src` and `tests` folders
-# are located.
-python3w -m pipenv shell
-mkdir "${reports_folder}"
-pytest --junit-xml="${reports_folder}/report.xml"
